6b56870c650a6885aa6b863d12369ae4345e7f97
Eden Judah Amsalem <eamsalem@c1r6s3.42london.com>
2025-03-19 16:59:30 +0000

    fixed atof issue with negative decimal < 1

--- atoi_fns/ft_atof.c
+++ atoi_fns/ft_atof.c
@@ -6,7 +6,7 @@
 /*   By: eamsalem <eamsalem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 12:22:29 by eamsalem          #+#    #+#             */
-/*   Updated: 2025/02/26 19:19:22 by eamsalem         ###   ########.fr       */
+/*   Updated: 2025/03/19 16:59:03 by eamsalem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,21 +46,44 @@ double	get_decimal_part(const char *str)
 	return (nbr);
 }
 
+int	get_int_part(const char *str, int i)
+{
+	int		nbr_int;
+
+	nbr_int = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr_int *= 10;
+		nbr_int += str[i] - '0';
+		i++;
+	}
+	return (nbr_int);
+}
 double	ft_atof(const char *str)
 {
 	double	nbr;
+	int		sign;
+	int		i;
 
-	nbr = ft_atoi(str);
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	sign = 1;
+	if (str[0] == '-')
+	{
+		i++;
+		sign = -1;
+	}
+	nbr = get_int_part(str, i);
 	nbr += get_decimal_part(str);
-	return (nbr);
+	return (nbr * sign);
 }
-/*
-int main(void)
-{
-	char *str = "0.0";
-	char *str1 = "-10.0";
 
-	printf("nbr = %f\n", ft_atof(str));
-	printf("nbr = %f\n", ft_atof(str1));
-}
-*/
+// int main(void)
+// {
+// 	char *str = "0.0";
+// 	char *str1 = "-0.5";
+
+// 	printf("nbr = %f\n", ft_atof(str));
+// 	printf("nbr = %f\n", ft_atof(str1));
+// }
